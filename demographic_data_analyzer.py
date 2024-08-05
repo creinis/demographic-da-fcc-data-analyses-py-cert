da05ca1145fb019704d61a7bc3dc9ff85ea2ff2d
creinis <115181527+creinis@users.noreply.github.com>
2024-08-05 15:00:28 +0200

    feat: fcc print data

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -38,6 +38,38 @@ def calculate_demographic_data(print_data=True):
       (num_min_workers[num_min_workers['salary'] == '>50K'].shape[0] /
        num_min_workers.shape[0]) * 100, 1)
     
+    # What country has the highest percentage of people that earn >50K?
+    highest_earning_country = (
+      df.loc[df['salary'] == '>50K', 'native-country'].value_counts() /
+      df['native-country'].value_counts()).idxmax()
+    highest_earning_country_percentage = round(
+      (df[(df['native-country'] == highest_earning_country) &
+          (df['salary'] == '>50K')].shape[0] /
+       df[df['native-country'] == highest_earning_country].shape[0]) * 100, 1)
+    
+
+
+    # DO NOT MODIFY BELOW THIS LINE
+
+    if print_data:
+        print("Number of each race:\n", race_count)
+        print("Average age of men:", average_age_men)
+        print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
+        #print(
+        #   f"Percentage with higher education that earn >50K: {higher_education_rich}%"
+        #)
+        #print(
+        #    f"Percentage without higher education that earn >50K: {lower_education_rich}%"
+        #)
+        print(f"Min work time: {min_work_hours} hours/week")
+        print(
+            f"Percentage of rich among those who work fewest hours: {rich_percentage}%"
+        )
+        #print("Country with highest percentage of rich:", highest_earning_country)
+        #print(
+        #    f"Highest percentage of rich people in country: {highest_earning_country_percentage}%"
+        #)
+        print("Top occupations in India:", top_IN_occupation)  
     
     
     return {
@@ -48,6 +80,7 @@ def calculate_demographic_data(print_data=True):
       'lower_education_rich': lower_education_rich,
       'min_work_hours': min_work_hours,
       'rich_percentage': rich_percentage,
+      'highest_earning_country': highest_earning_country,
     }
 
 
