31161481c40cc762fc159c789396935e6f14bfb6
creinis <115181527+creinis@users.noreply.github.com>
2024-08-05 14:42:41 +0200

    feat: How many people of each race are represented in this dataset? This should be a Pandas series with race names as the index labels. (race column)

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -5,3 +5,8 @@ def calculate_demographic_data(print_data=True):
     # Read data from file
     df = pd.read_csv('adult.data.csv')
 
+    # How many of each race are represented in this dataset?
+    # This should be a Pandas series with race names as the index labels.
+    race_count = df['race'].value_counts()
+    
+    
